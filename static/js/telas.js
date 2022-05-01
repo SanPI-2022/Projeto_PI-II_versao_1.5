09ff1b8aa85763daf2936c6b5232e8647c278a68
SanPI-2022 <104701576+SanPI-2022@users.noreply.github.com>
2022-04-30 21:56:42 -0300

    Add files via upload

---
+++ static/js/telas.js
@@ -0,0 +1,27 @@
+function mudaTela1(){
+	    window.location.href = "tela2.html"
+	}
+
+	function mudaTela2(){
+	    window.location.href = "tela3.html"
+	}
+
+	function mudaTela3(){
+	    window.location.href = "tela4.html"
+	}
+
+	function mudaTela4(){
+	    window.location.href = "tela5.html"
+	}
+
+	function mudaTela5(){
+	    window.location.href = "tela1.html"
+	}
+
+	function mudaTelaN(){
+	    window.location.href = "tela2.html"
+	}
+
+	function mudaTelaC(){
+	    window.location.href = "tela1.html"
+	}
